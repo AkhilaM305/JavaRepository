85a117479173781d0d46ae4b6a325e6fd26fa162
MANAKHIL <MANAKHIL@INLEN8520075759.corp.capgemini.com>
2023-02-09 16:50:10 +0530

    updated with mockito and loggers

--- ExceptionHandling/src/Calculator.java
+++ ExceptionHandling/src/Calculator.java
@@ -1,17 +1,40 @@
-
+class CountByZeroException extends Exception{
+	private String message;
+	public CountByZeroException() {}
+	public CountByZeroException(String message) {
+		this.message=message;
+	}
+	public void setMessage(String message) {
+		this.message=message;
+	}
+	public String getMessage() {
+		return this.message;
+	}
+}
 public class Calculator {
-	public static void doAverage(int...a) throws CountByZeroException{
-	float average;
-	int sum=0;
-	for(int x:a) {
-		sum+=x;
+	public static void doAverage(int...a)throws CountByZeroException{
+		float average = 0;
+		int sum = 0;
+		for(int x:a) {
+			sum+=x;
+		}
+		if(a.length==0) {
+			throw new CountByZeroException("numbers not found");
+		}
+		average = sum/a.length;
+		System.out.println("average is" + average);
 	}
-	if(a.length==0) {
-		throw new CountByZeroException{"Zero count found"};
-	}
-	
-		average=sum/a.length;
-		System.out.println("Average"+ average);
-	}
+}
+class Source{
+  public static void main(String args[]) {
+	  try {
+	Calculator.doAverage(2,5,7);
+	Calculator.doAverage(7,8,9);
+	Calculator.doAverage();
+}
+	  catch(CountByZeroException message) {
+		  System.out.println(message);
+	  }
+}
 }
 
